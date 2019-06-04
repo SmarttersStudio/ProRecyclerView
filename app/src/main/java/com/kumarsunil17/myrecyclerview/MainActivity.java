0e4502cf0c9b019e4320b4f908d5304bd9f655f3
KumarSunil17 <42208638+KumarSunil17@users.noreply.github.com>
2019-06-04 12:24:37 +0530

    initial commit

--- app/src/main/java/com/kumarsunil17/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/kumarsunil17/myrecyclerview/MainActivity.java
@@ -23,58 +23,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        recyclerView = findViewById(R.id.container);
-//        l = new ArrayList<>();
-//
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView.setAdapter(new MainAdapter(l,this));
-//        recyclerView.addLoaderLayout(R.layout.shimmer_loader);
-        //recyclerView.showLoader();
+
+        recyclerView = findViewById(R.id.container);
+        l = new ArrayList<>();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new MainAdapter(l,this));
+        recyclerView.addLoaderLayout(R.layout.shimmer_loader);
+ //       recyclerView.showLoader();
         //recyclerView.showError();
-//        recyclerView.showEmpty();
-//        recyclerView.hideRecycler();
-//
-//        for (int i=97;  i<100;  i++){
-//            l.add(String.valueOf((char) i));
-//        }
-//
-//        recyclerView.onReload(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "Reloading", Toast.LENGTH_SHORT).show();
-//
-//                recyclerView.hideEmpty();
-//                recyclerView.showRecycler();
-//            }
-//        });
-//        recyclerView.onRetry(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "Retrying", Toast.LENGTH_SHORT).show();
-//
-//                if (l.size()==0) {
-//                    recyclerView.showEmpty();
-//                    recyclerView.hideRecycler();
-//                }
-//                recyclerView.hideError();
-//            }
-//        });
-//        recyclerView.setOnMoreListener(new OnMoreListener() {
-//            @Override
-//            public void onMoreAsked(int overallItemsCount, int maxLastVisiblePosition) {
-//
-//            }
-//        });
-//        recyclerView.setRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                for (int i=100;  i<120;  i++){
-//                    l.add(String.valueOf((char) i));
-//                }
-//                recyclerView.setAdapter(new MainAdapter(l,MainActivity.this));
-//                recyclerView.setRefreshing(false);
-//            }
-//        });
+        recyclerView.showEmpty();
+        recyclerView.hideRecycler();
+
+        for (int i=97;  i<100;  i++){
+            l.add(String.valueOf((char) i));
+        }
+
+        recyclerView.onReload(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Reloading", Toast.LENGTH_SHORT).show();
+
+                recyclerView.hideEmpty();
+                recyclerView.showRecycler();
+            }
+        });
+        recyclerView.onRetry(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Retrying", Toast.LENGTH_SHORT).show();
+
+                if (l.size()==0) {
+                    recyclerView.showEmpty();
+                    recyclerView.hideRecycler();
+                }
+                recyclerView.hideError();
+            }
+        });
+        recyclerView.setOnMoreListener(new OnMoreListener() {
+            @Override
+            public void onMoreAsked(int overallItemsCount, int maxLastVisiblePosition) {
+
+            }
+        });
+        recyclerView.setRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                for (int i=100;  i<120;  i++){
+                    l.add(String.valueOf((char) i));
+                }
+                recyclerView.setAdapter(new MainAdapter(l,MainActivity.this));
+                recyclerView.setRefreshing(false);
+            }
+        });
+
     }
 }
